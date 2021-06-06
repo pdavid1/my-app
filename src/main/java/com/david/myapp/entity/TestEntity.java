feaec706e945fa181ae6950dde72180679fa9764
Carlos Altamirano <alt.carlos@gmail.com>
2021-06-05 21:44:50 -0400

    Nombre del Proyecto: my-app, Descripcion: Versión final del proyecto de prueba

---
+++ src/main/java/com/david/myapp/entity/TestEntity.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.david.myapp.entity;
+
+/**
+ * @author HP
+ *
+ */
+public class TestEntity {
+	/**
+	 * Propiedad de prueba para la muestra de complicación del archivo java
+	 */
+	private String nombre;
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+}
